4e28f921090d9c6a9c2a30d0abc928026fe5f038
Benz Samson Tagle <benztagle123@gmail.com>
2023-08-29 15:18:09 +0800

    Optimized if/else logic for changeSignFunction. toggle boolean variable is redundant and been removed

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// Benz Samson Tagle
+// The Odin Project: Calculator Project Using HTML, CSS, VANILLA JAVASCRIPT
+
 const buttons = document.querySelectorAll('[data-number]');
 const currentOperand = document.querySelector('.currentNumDisplay');
 const previousOperand = document.querySelector('.previousNumDisplay');
@@ -13,7 +16,7 @@ let firstNum = null;
 let secondNum = null;
 let operatorIsClick = false;
 
-// Event Listeners
+// Event Listeners  // Event Listeners  // Event Listeners  // Event Listeners  // Event Listeners
 buttons.forEach(button => {
     button.addEventListener("click", numberDisplay)
 })
@@ -32,37 +35,50 @@ decimalButton.addEventListener("click", decimal)
 
 changeSignButton.addEventListener("click", changeSignFunction)
 
+// Functions  // Functions  // Functions  // Functions  // Functions  // Functions  // Functions
+// +-*/ operator button function
+function operatorFunction(){
+    operator = this.textContent;
+    firstNum = currentOperand.textContent;
 
-let toggle = true;
+    if(operatorIsClick) {
+        let test = previousOperand.textContent.slice(0, -1);
+        previousOperand.textContent = test + operator;
+        firstNum = test;
+        console.log(test);
 
-// Functions
+        return
+    }
+
+    if(secondNum === null) return
+
+    operatorIsClick = true;
+    previousOperand.textContent = currentOperand.textContent + ' ' + operator;
+    currentOperand.textContent = '';
+    secondNum = null;
+                                           /* Console.log firstNum, secondNum, operator*/   console.log(`first num: ${firstNum}`);  console.log(`second num: ${secondNum}`); console.log(`operator: ${operator}`);console.log(`              `);                                                      
+}//end of operatorFunction() 
+
 function changeSignFunction(){
      if(currentOperand.textContent.slice(0,1) === '-'){
         currentOperand.textContent = currentOperand.textContent.substring(1)
-     } else if(toggle){
+     } else if(!(currentOperand.textContent.slice(0,1) === '-')){
         currentOperand.textContent = '-' + currentOperand.textContent
-        toggle = true;
      }
-}
+}//end of changeSignFunction()
 
-function equal(){
+// Equal button function
+function equal(){       
     if(secondNum === null) return
     secondNum = currentOperand.textContent;
     
     if(operator === '/' && (firstNum == 0 || secondNum == 0)) alert('Cannot divide with zero!')
+                                           /* Console.log firstNum, secondNum, operator*/   console.log(`first num: ${firstNum}`);  console.log(`second num: ${secondNum}`); console.log(`operator: ${operator}`);console.log(`              `);                                                      
+ currentOperand.textContent = operate(operator, parseInt(firstNum), parseInt(secondNum));
+}//end of equal()
 
-                                                                                            console.log(`first num: ${firstNum}`)
-                                                                                            console.log(`second num: ${secondNum}`)
-                                                                                            console.log(`operator: ${operator}`)
-                                                                                            console.log(`              `);
-
-
-    currentOperand.textContent = operate(operator, parseInt(firstNum), parseInt(secondNum));
-
-}
-
+// Main operation function. This checks what operator was used and returns the answer.
 function operate(operatorSymbol, a, b){
-
     switch(operatorSymbol) {
         case '+' : return a + b
 
@@ -73,75 +89,36 @@ function operate(operatorSymbol, a, b){
         case '/' : return a / b
 
         default : return null
-    }
-}
+    }                             // Notice that there is no 'break;' statement, it is because each case returns an answer, the return statement ends the process itself.
+}//end of operate()
 
+// Delete button function
 function del(){
     currentOperand.textContent = currentOperand.textContent.slice(0, -1)
     operatorIsClick = false;
-    toggle = true;
 }
 
+// AC button function
 function allClear(){
-    console.log("working")
     currentOperand.textContent = '';
     previousOperand.textContent = '';
+    operatorIsClick = false;
     firstNum = null;
     secondNum = null;
     operator = null;
-    operatorIsClick = false;
-    toggle = true;
-
-                                                                                                console.log(`first num: ${firstNum}`)
-                                                                                                console.log(`second num: ${secondNum}`)
-                                                                                                console.log(`operator: ${operator}`)
-                                                                                                console.log(`              `);
+                                           /* Console.log firstNum, secondNum, operator*/   console.log(`first num: ${firstNum}`);  console.log(`second num: ${secondNum}`); console.log(`operator: ${operator}`);console.log(`              `);                                                     
 }
 
-function operatorFunction(){
-    // if(currentOperand.textContent === '') return;
-    operator = this.textContent;
-    firstNum = currentOperand.textContent;
-    
-    if(operatorIsClick) {
-        let test = previousOperand.textContent.slice(0, -1);
-        previousOperand.textContent = test + operator;
-        firstNum = test;
-        console.log(test);
-
-        return
-    }
-    
-    if(secondNum === null) return
-
-    operatorIsClick = true;
-    previousOperand.textContent = currentOperand.textContent + ' ' + operator;
-    currentOperand.textContent = '';
-    secondNum = null;
-
-    console.log(`first num: ${firstNum}`)
-    console.log(`second num: ${secondNum}`)
-    console.log(`operator: ${operator}`)
-    console.log(`              `);
-
-}
-
-function numberDisplay(){
-    currentOperand.textContent += this.textContent;
-    operatorIsClick = false;
-    secondNum = currentOperand.textContent;
-
-
-                                                                                            console.log(`first num: ${firstNum}`)
-                                                                                            console.log(`second num: ${secondNum}`)
-                                                                                            console.log(`operator: ${operator}`)
-                                                                                            console.log(`              `);
-
-
-}
-
+// decimal button function
 function decimal(){
     if(currentOperand.textContent.includes('.')) return;
     currentOperand.textContent += this.textContent;
 }
 
+    // Current number being displayed
+function numberDisplay(){
+    currentOperand.textContent += this.textContent;
+    secondNum = currentOperand.textContent;
+    operatorIsClick = false;
+                                           /* Console.log firstNum, secondNum, operator*/   console.log(`first num: ${firstNum}`);  console.log(`second num: ${secondNum}`); console.log(`operator: ${operator}`);console.log(`              `);                                                      
+}  
